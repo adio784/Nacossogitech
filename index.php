48a4feb2ef19a31b7b728813b45b40e5abc3d500
adio784 <adioridwan784@gmail.com>
2019-10-18 09:13:50 +0100

    update

---
+++ index.php
@@ -0,0 +1,254 @@
+<!DOCTYPE html>
+<html>
+<head>
+	  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	  <link rel="shortcut icon" href="css/img/nac3.jpg" type="image/x-icon" />
+	  <link rel="stylesheet" type="text/css" href="css/main.css">
+      
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<title> :: Welcome to NACOSS OGITECH </title>
+
+</head>
+<body>
+    <div class="nav black">
+    <div class="container white-text" style="width: 80%; height:25px;">
+      <a href="#!" class=" white-text" ><i class="fa fa-car white-text"></i> +234-7035743427 </a> &nbsp;
+      <a href="#!" class=" white-text" ><i class="fa fa-car"></i> contact@nacossogitech.com</a>
+      <a href="#!" class=" white-text" ><i class="fa fa-car"></i> Current time: </a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a><i class="fa fa-car"></i> </a></li>
+        <li><a href="badges.html"><i class="fa fa-car"></i></a></li>
+        <li><a href="collapsible.html"><i class="fa fa-car"></i></a></li>
+      </ul>	
+    </div>
+
+    </div>
+
+	
+<div class="nav-fixed">
+  <nav class="grey lighten-4">
+    <div class="container">
+    	<div class="nav-wrapper">
+	       <a href="#" class="brand-logo">NACOSS Ogitech</a>
+	       <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+	       <ul class="right hide-on-med-and-down">
+	        <li><a href="#home">Home</a></li>
+	        <li><a href="#student">Students</a></li>
+	        <li><a href="#department">Department</a></li>
+	        <li><a href="#gallery">Gallery</a></li>
+	        <li><a href="#login">Login</a></li>
+	       </ul>		
+	    	</div>
+    </div>
+  </nav>
+</div>
+	       <ul class="sidenav" id="mobile-demo">
+	        <li><a href="#home">Home</a></li>
+	        <li><a href="#student">Students</a></li>
+	        <li><a href="#department">Department</a></li>
+	        <li><a href="#gallery">Gallery</a></li>
+	        <li><a href="#login">Login</a></li>
+	       </ul> 
+
+<!-- Slidder -->
+<section class="slider">
+	<ul class="slides">
+		<li>
+			<img src="css/img/img1.jpg">
+			<div class="caption left-align">
+				<h2>Take your Dream Vacation</h2>
+				<h5 class="light grey-text text-lighten-3 hide-on-small-only">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h5>
+				<a href="#" class="btn btn-large">Learn more</a>
+			</div>
+		</li>
+		<li>
+			<img src="css/img/img2.jpg">
+			<div class="caption right-align">
+				<h2>Take your Dream Vacation</h2>
+				<h5 class="light black-text text-darken-2 hide-on-small-only">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h5>
+				<a href="#" class="btn btn-large">Learn more</a>
+			</div>
+		</li>
+		<li>
+			<img src="css/img/img3.jpg">
+			<div class="caption center-align">
+				<h2>Take your Dream Vacation</h2>
+				<h5 class="light grey-text text-lighten-3 hide-on-small-only">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h5>
+				<a href="#" class="btn btn-large">Learn more</a>
+			</div>
+		</li>
+		<li>
+			<img src="css/img/img4.jpg">
+			<div class="caption">
+				<h2>Take your Dream Vacation</h2>
+				<h5 class="light grey-text text-lighten-3 hide-on-small-only">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h5>
+				<a href="#" class="btn btn-large">Learn more</a>
+			</div>
+		</li>
+		<li>
+			<img src="css/img/img5.jpg">
+			<div class="caption">
+				<h2>Take your Dream Vacation</h2>
+				<h5 class="light grey-text text-lighten-3 hide-on-small-only">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h5>
+				<a href="#" class="btn btn-large">Learn more</a>
+			</div>
+		</li>
+		<li>
+			<img src="css/img/img6.jpg">
+			<div class="caption">
+				<h2>Take your Dream Vacation</h2>
+				<h5 class="light grey-text text-lighten-3 hide-on-small-only">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h5>
+				<a href="#" class="btn btn-large">Learn more</a>
+			</div>
+		</li>
+	</ul>
+</section>
+
+<!-- Search Sections -->
+<section class="section section-search green accent-4 white-text center">
+	<div class="container">
+		<div class="row">
+			<div class="col s12"></div>
+			<h3>We Provide The IT Solutions</h3>
+			<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+		</div>
+	</div>
+</section>
+<!-- Search Sections -->
+<section class="section section-icons grey lighten-5 lighten-4 center">
+	<div class="container">
+		<div class="row">
+			<div class="col s12 m4 ">
+				<div class="card-panel">
+					<i class="material-icons large teal-text">room</i>
+					<h4>Machine Learning</h4>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat.</p>
+				</div>
+			</div>
+			<div class="col s12 m4 ">
+				<div class="card-panel">
+					<i class="material-icons large teal-text">store</i>
+					<h4>Data Science</h4>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat.</p>
+				</div>
+			</div>
+			<div class="col s12 m4 ">
+				<div class="card-panel">
+					<i class="material-icons large teal-text">room</i>
+					<h4>Digital Marketting</h4>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- Gallery -->
+<section class="section section-gallery">
+	<div class="container">
+		<h4 class="center"><span class="green-text accent-4">Photo </span>GALLERY</h4>
+		<div class="row">
+			<div class="col s12 m3">
+				<img class="materialboxed responsive-img" src="https://source.unsplash.com/1600x900?beach" alt="">
+			</div>
+			<div class="col s12 m3">
+				<img class="materialboxed responsive-img" src="https://source.unsplash.com/1600x900?travel" alt="">
+			</div>
+			<div class="col s12 m3">
+				<img class="materialboxed responsive-img" src="https://source.unsplash.com/1600x900?nature" alt="">
+			</div>
+			<div class="col s12 m3">
+				<img class="materialboxed responsive-img" src="https://source.unsplash.com/1600x900?beach,travel" alt="">
+			</div>
+		</div>
+		<div class="row">
+			<div class="col s12 m3">
+				<img class="materialboxed responsive-img" src="https://source.unsplash.com/1600x900?beach" alt="">
+			</div>
+			<div class="col s12 m3">
+				<img class="materialboxed responsive-img" src="https://source.unsplash.com/1600x900?travel" alt="">
+			</div>
+			<div class="col s12 m3">
+				<img class="materialboxed responsive-img" src="https://source.unsplash.com/1600x900?nature" alt="">
+			</div>
+			<div class="col s12 m3">
+				<img class="materialboxed responsive-img" src="https://source.unsplash.com/1600x900?beach,travel" alt="">
+			</div>
+		</div>
+		<div class="row">
+			<div class="col s12 m3">
+				<img class="materialboxed responsive-img" src="https://source.unsplash.com/1600x900?beach" alt="">
+			</div>
+			<div class="col s12 m3">
+				<img class="materialboxed responsive-img" src="https://source.unsplash.com/1600x900?tree" alt="">
+			</div>
+			<div class="col s12 m3">
+				<img class="materialboxed responsive-img" src="https://source.unsplash.com/1600x900?nature" alt="">
+			</div>
+			<div class="col s12 m3">
+				<img class="materialboxed responsive-img" src="https://source.unsplash.com/1600x900?travel" alt="">
+			</div>
+		</div>
+
+	</div>
+</section>
+<!-- Gallery -->
+
+
+<!-- Slidder -->
+<div class="row">
+	<div class="col s12 m6">
+		
+	</div>
+</div>
+<div class="row">
+	<div class="col s12 m9">
+		feeeddededeefdeef
+	</div>
+	<div class="col s12 m3">News Feed</div>
+</div>
+
+<!--JavaScript at end of body for optimized loading-->
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/materialize.min.js"></script>
+<script type="text/javascript" src="js/materialize.js"></script>
+
+		
+
+
+<script>
+			  
+	$(document).ready(function(){
+	$('.sidenav').sidenav();
+  
+ 
+    $('.carousel-slider').carousel({fullWidth:true});
+    $('.slider').slider({
+    	indicators: false,
+    	height: 500,
+    	transition: 500,
+    	interval: 6000,
+    });
+
+  });
+		     
+</script>
+
+</body>
+
+</html>
